8a3c57ebc13544e3e9beccba5393238eb5c75040
Andrew Gulin <andrewgulin@MacBook-Air-Andrew.local>
2021-12-29 16:50:19 +0300

    [+] react as dependency (fix), add import from module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import BaseRootStore from "./stores/BaseRootStore";
 import BaseSubstore from "./stores/BaseSubstore";
+import BaseUserStore from './stores/BaseUserStore';
 import { ApiBaseAuthType, ApiBaseUserType, ApiSexType } from "./stores/types/api";
+import { UrlsType, UrlTuple } from './stores/types/stores';
 
 export {
   BaseRootStore,
   BaseSubstore,
+  BaseUserStore,
 
   ApiBaseAuthType,
   ApiBaseUserType,
   ApiSexType,
+
+  UrlsType,
+  UrlTuple
 };
